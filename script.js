8fbeb076d6f7f558d89aaef71dd57e289a5eacb2
Dima <dima.kulik.2234@gmail.com>
2023-01-08 14:26:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// console.log( 5 - 4 )
+// console.log(54 + +'87')
+// let Age = prompt('Скільки тобі років','')
+// function name (age) {
+//     if (age < 18 ) {
+//         alert('Мені ' + age + ' бо я ше синок')
+//     } if (age > 18) {
+//         alert('Мені ' + age + ' бо я вже великий')
+//     }
+// };
+// name(Age);
+
+
+// let answers = [];
+//
+// answers[0] = prompt('Як тебе звать?','No name')
+//
+// for ( let i = 0; i < 3; i++ ) {
+//      answers[i] = prompt('Як тебе звать?','')
+// }
+// console.log(answers);
+
+// let answers = []
+//
+// answers[0] = prompt('Як тебе звати ?', '')
+// answers[1] = prompt('Яка в тебе фамілія ?', '')
+// answers[2] = prompt('Скільки тобі років ?', '')
+//
+// console.log(typeof(answers))
+// let Name = prompt('Як тебе звать?','')
+// console.log(Name)
